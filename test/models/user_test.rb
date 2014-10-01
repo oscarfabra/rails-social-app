9ec2f54c91c1e707a53437aab3ae98cb56f7f252
Oscar Fabra <oscarfabra@gmail.com>
2014-09-30 21:37:07 -0500

    Modeled users and added validations on model and db for email attr

---
+++ test/models/user_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  # If @user.name = "" then @user.valid? should return false
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  # If @user.length is > 50 then @user.valid? should return false
+  test "name shouldn't be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  # Verifies that rare but valid email addresses are accepted
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?
+    end
+  end
+
+  # Verifies that invalid email addresses are not accepted
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?
+    end
+  end
+
+  # Verifies that two emails with same chars but different cases are treated as
+  # distinct emails
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
