7ad0ed1ed204a1ec9a5c12b896dd10da9dd5f373
Oscar Fabra <oscarfabra@gmail.com>
2014-10-01 15:20:24 -0500

    Added debug box at the bottom of all pages

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -41,7 +41,7 @@ class UserTest < ActiveSupport::TestCase
   # Verifies that invalid email addresses are not accepted
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-      foo@bar_baz.com foo@bar+baz.com]
+      foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?
@@ -55,6 +55,13 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPLE.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should have a minimum length" do
