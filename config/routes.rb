af8bc89097f41bfeda269237a413d9a63c9d65ca
Oscar Fabra <oscarfabra@gmail.com>
2014-10-01 22:16:15 -0500

    Added sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
+  # Routes for static pages
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+
+  # Routes for the users resource
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
