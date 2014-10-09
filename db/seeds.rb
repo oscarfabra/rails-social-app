87a3aa7f8d0b157654b90b84392c965cd0fa18d9
Oscar Fabra <oscarfabra@gmail.com>
2014-10-08 19:23:59 -0500

    Added microposts model, views, controller & user profile test

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,3 +42,10 @@ for n in 100..102
     password_confirmation: password, 
     activated: false)
 end
+
+# Adds microposts to the first 6 created users
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
---
+++ test/integration/users_profile_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersProfileTest < ActionDispatch::IntegrationTest
+  
+  # Include ApplicationHelper to access its helper methods
+  include ApplicationHelper
+
+  def setup
+    @user = users(:michael)    
+  end
+
+  test "profile display" do
+    # Visit the user profile
+    get user_path(@user)
+    assert_select 'title', full_title(@user.name)
+    # The user's name, image, microposts' count are in the HTML body
+    assert_match @user.name, response.body
+    assert_select 'img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    # and there's pagination of microposts
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
+end
