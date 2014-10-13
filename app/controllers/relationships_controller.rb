79df215223551dceee221c9c08e09ea337c4a4d6
Oscar Fabra <oscarfabra@gmail.com>
2014-10-12 20:07:28 -0500

    Added working follow/unfollow ajax-enabled buttons.

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,29 @@
+class RelationshipsController < ApplicationController
+  
+  # Requires user to be logged in before any action
+  before_action :logged_in_user
+
+  def create    
+    # Finds user to follow
+    @user = User.find(params[:followed_id])
+    # Follow user
+    current_user.follow(@user)
+    # Redirects to the other user's profile
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
+  def destroy
+    # Finds user to destroy
+    @user = Relationship.find(params[:id]).followed
+    # Unfollows user
+    current_user.unfollow(@user)
+    # Redirects to the other user's profile
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+end
--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:michael)    
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -27,5 +28,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
     end
-  end  
+  end
+
+  test "should follow a user" do
+    # Verify following count increments by one, normal post
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, followed_id: @other.id
+    end
+    # Unfollow the other user to restart the test
+    @user.unfollow(@other)
+    # Verify following count increments by one, xhr post
+    assert_difference '@user.following.count', 1 do
+      xhr :post, relationships_path, followed_id: @other.id
+    end
+  end
+
+  test "should unfollow a user" do
+    # Verify following count decreases by one, normal post
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), relationship: relationship.id
+    end
+    # Follow the other user to restart the test
+    @user.follow(@other)
+    # Verify following count decreases by one, xhr post
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship), relationship: relationship.id
+    end
+  end
 end
