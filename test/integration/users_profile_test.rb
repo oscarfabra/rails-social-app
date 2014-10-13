e8b65fddf799fd697de7a67202f23cc6723bd175
Oscar Fabra <oscarfabra@gmail.com>
2014-10-12 22:19:35 -0500

    Added tests for microposts on user's home page and profile.

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -22,5 +22,9 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    # The user stats are present in the HTML
+    assert_select 'section.stats'
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
