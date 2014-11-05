127a1db54e36b6b6e6eda1096cb2da7e03a7ec3e
Oscar Fabra <oscarfabra@gmail.com>
2014-11-04 21:45:24 -0500

    Finally could fix landing page. Polished btns, forms and profiles. Contact form lacking...

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,9 +1,6 @@
 /* Functions used by static_pages */
 
 var main = function() {
-  $('.dropdown-toggle').click(function() {
-    $('.dropdown-menu').toggle();
-  });
   
   $('.arrow-next').click(function() {
     var currentSlide = $('.active-slide');
@@ -17,8 +14,8 @@ var main = function() {
       nextDot = $('.dot').first();
     }
     
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    nextSlide.fadeIn(600).addClass('active-slide');
+    currentSlide.fadeOut(700).removeClass('active-slide');
+    nextSlide.fadeIn(700).addClass('active-slide');
 
     currentDot.removeClass('active-dot');
     nextDot.addClass('active-dot');
@@ -36,8 +33,8 @@ var main = function() {
       prevDot = $('.dot').last();
     }
     
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    prevSlide.fadeIn(600).addClass('active-slide');
+    currentSlide.fadeOut(700).removeClass('active-slide');
+    prevSlide.fadeIn(700).addClass('active-slide');
 
     currentDot.removeClass('active-dot');
     prevDot.addClass('active-dot');
