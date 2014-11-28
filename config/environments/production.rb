24fba47cc4fe513fdb34e767cbf508224468a805
Oscar Fabra <oscarfabra@gmail.com>
2014-11-27 22:15:33 -0500

    Mailer typo in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Use SendGrid in production.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ruby-sample-app.herokuapp.com'
+  host = 'rails-social-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = 
   {
